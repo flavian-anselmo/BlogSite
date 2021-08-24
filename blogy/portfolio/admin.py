ce7223d79e05970f20f4825f398c6b91346e69fe
flavian-anselmo <anselmoflavian4@gmail.com>
2021-08-24 12:54:53 +0300

    created the admin panel for the application by registering th models to the django admin panel

--- blogy/portfolio/admin.py
+++ blogy/portfolio/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from portfolio.models  import Project
 
 # Register your models here.
+class PostProject(admin.ModelAdmin):
+    pass
+admin.site.register(Project,PostProject)
+"""
+creating the admin panel for the project 
+we have to register the data models that are
+like our tables in the database in to the Admin
+THis allows as to have input fields in the admin panel
+for us to update the content dynamically  
+"""
